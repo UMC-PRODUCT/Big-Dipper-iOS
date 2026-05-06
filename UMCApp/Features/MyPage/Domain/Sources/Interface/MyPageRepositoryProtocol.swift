fc3a50187f297e5acdf42bf995e01b1e4513b89f
GthingkingG <21thingking@gmail.com>
2026-05-07 07:39:21 +0900

    feat: 마이페이지 terms reopository 구현

--- UMCApp/Features/MyPage/Domain/Sources/Interface/MyPageRepositoryProtocol.swift
+++ UMCApp/Features/MyPage/Domain/Sources/Interface/MyPageRepositoryProtocol.swift
@@ -6,3 +6,10 @@
 //
 
 import Foundation
+
+/// MyPage 데이터 접근 Repository Protocol
+public protocol MyPageRepositoryProtocol: Sendable {
+    
+    /// 약관 타입으로 약관 링크 정보를 조회합니다.
+    func fetchTerms(termsType: String) async throws -> MyPageTerms
+}
