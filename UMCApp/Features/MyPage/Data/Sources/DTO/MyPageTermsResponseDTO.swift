aa6ad370ca325e7f9a1d66647093d24110238539
GthingkingG <21thingking@gmail.com>
2026-05-07 07:39:40 +0900

    feat: 마이페이지 terms 데이터 구현

---
+++ UMCApp/Features/MyPage/Data/Sources/DTO/MyPageTermsResponseDTO.swift
@@ -0,0 +1,23 @@
+//
+//  MyPageTermsResponseDTO.swift
+//  MyPageData
+//
+//  Created by One on 5/6/26.
+//
+
+import Foundation
+
+
+public struct MyPageTermsResponseDTO: Codable {
+    let id: String
+    let link: String
+    let isMandatory: Bool
+    
+    public func toDomain() -> MyPageTerms {
+        MyPageTerms(
+            id: id,
+            link: link,
+            isMandatory: isMandatory
+        )
+    }
+}
