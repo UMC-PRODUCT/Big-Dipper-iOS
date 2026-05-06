fe4498444a286bd33ee1ce9e0c828f2a763826f3
GthingkingG <21thingking@gmail.com>
2026-05-07 07:40:22 +0900

    feat: 마이페이지 terms 라우터 구현

--- UMCApp/Features/MyPage/Data/Sources/Router/MyPageRouter.swift
+++ UMCApp/Features/MyPage/Data/Sources/Router/MyPageRouter.swift
@@ -6,3 +6,38 @@
 //
 
 import Foundation
+import Alamofire
+import Moya
+import CoreNetwork
+
+/// 마이페이지 Feature API 라우터
+public enum MyPageRouter {
+    case getTerms(termsType: String)
+}
+
+// MARK: - BaseTargetType
+
+extension MyPageRouter: BaseTargetType {
+    public var path: String {
+        switch self {
+        case .getTerms(let termsType):
+            return "/api/v1/terms/type/\(termsType)"
+        }
+    }
+    
+    public var method: Moya.Method {
+        switch self {
+        case .getTerms:
+            return .get
+        }
+    }
+    
+    public var task: Moya.Task {
+        switch self {
+        case .getTerms:
+            return .requestPlain
+        }
+    }
+    
+    
+}
