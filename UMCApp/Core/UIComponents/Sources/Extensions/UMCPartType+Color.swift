1dc504cc99b40b977e6e9f9745667cfe7598d8ea
leeyeji <yeji1135@gachon.ac.kr>
2026-05-09 02:32:08 +0900

    refactor: UMCPartType을 UMCFoundation으로 이관, 색상 확장을 CoreUIComponents로 분리
      - UMCPartType 정의를 UMCFoundation/Types로 추가 (도메인 간 공유 타입)
      - UMCPartType+Color 확장을 CoreDesignSystem → CoreUIComponents/Extensions로 이관
        CoreDesignSystem은 UMCFoundation을 알지 못하므로 잘못된 위치였음
      - CoreUIComponents/Project.swift에 UMCFoundation 의존성 추가

---
+++ UMCApp/Core/UIComponents/Sources/Extensions/UMCPartType+Color.swift
@@ -0,0 +1,34 @@
+//
+//  UMCPartType+Color.swift
+//  CoreDesignSystem
+//
+//  Created by 이예지 on 5/8/26.
+//
+
+import SwiftUI
+import UMCFoundation
+
+extension UMCPartType {
+    /// 파트별 고유 색상
+    public var color: Color {
+        switch self {
+        case .admin:
+            return .indigo
+        case .pm:
+            return .purple
+        case .design:
+            return .pink
+        case .server(let type):
+            switch type {
+            case .spring: return .green
+            case .node:   return .yellow
+            }
+        case .front(let type):
+            switch type {
+            case .web:     return .brown
+            case .android: return .teal
+            case .ios:     return .orange
+            }
+        }
+    }
+}
