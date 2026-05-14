180f85dd6f7f841ead8436b39f3f18563f26802f
GthingkingG <21thingking@gmail.com>
2026-05-14 21:36:11 +0900

    feat: MyPageTermsResponseDTD에 custom init 작성

--- UMCApp/Features/MyPage/Data/Sources/DTO/MyPageTermsResponseDTO.swift
+++ UMCApp/Features/MyPage/Data/Sources/DTO/MyPageTermsResponseDTO.swift
@@ -14,6 +14,15 @@ public struct MyPageTermsResponseDTO: Codable {
     let link: String
     let isMandatory: Bool
     
+    public init(from decoder: any Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.id = try container.decode(String.self, forKey: .id)
+        self.link = try container.decode(String.self, forKey: .link)
+        self.isMandatory = try container.decode(Bool.self, forKey: .isMandatory)
+    }
+    
+    
+    
     public func toDomain() -> MyPageTerms {
         MyPageTerms(
             id: id,
