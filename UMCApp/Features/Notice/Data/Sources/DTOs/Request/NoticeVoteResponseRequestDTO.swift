8516199ffb01ede37c94c892f975221bf2023813
leeyeji <yeji1135@gachon.ac.kr>
2026-05-17 05:20:50 +0900

    refactor: NoticeVoteResponseRequestDTO 이식

---
+++ UMCApp/Features/Notice/Data/Sources/DTOs/Request/NoticeVoteResponseRequestDTO.swift
@@ -0,0 +1,18 @@
+//
+//  NoticeVoteResponseRequestDTO.swift
+//  NoticeData
+//
+//  Created by 이예지 on 5/17/26.
+//
+
+import Foundation
+import NoticeDomain
+
+/// 공지 투표 응답(제출/수정) 요청 DTO
+///
+/// - `POST /api/v1/notices/{noticeId}/votes/responses`
+/// - `PUT /api/v1/notices/{noticeId}/votes/responses`
+public struct NoticeVoteResponseRequestDTO: Encodable {
+    /// 사용자가 선택한 옵션 ID 목록
+    public let optionIds: [Int]
+}
