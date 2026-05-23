1a004aecaacf828e584cb2d5fe062c7b480a1b35
GthingkingG <21thingking@gmail.com>
2026-05-24 00:11:57 +0900

    chore: 의존성 주입

--- UMCApp/Features/MyPage/Project.swift
+++ UMCApp/Features/MyPage/Project.swift
@@ -26,5 +26,12 @@ let project = featureProject(
         .target(name: "MyPageDomain"),
         .project(target: "CoreNetwork", path: .relativeToRoot("Core/Network")),
         .project(target: "UMCFoundation", path: .relativeToRoot("Core/Foundation"))
+    ],
+    includesPresentationTests: true,
+    presentationTestDependencies: [
+        .project(target: "CoreDI", path: .relativeToRoot("Core/DI")),
+        .project(target: "CoreEnum", path: .relativeToRoot("Core/Enum")),
+        .project(target: "CoreDomain", path: .relativeToRoot("Core/Domain")),
+        .project(target: "UMCFoundation", path: .relativeToRoot("Core/Foundation")),
     ]
 )
