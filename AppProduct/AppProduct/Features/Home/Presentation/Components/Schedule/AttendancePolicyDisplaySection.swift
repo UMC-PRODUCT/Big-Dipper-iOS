4afe570e0a01a02f97aa5a53e14b75bb1cb0a2c2
JEONG <ikejhc159@gmail.com>
2026-06-02 08:15:27 +0900

    💄 [Design] 일정 상세 섹션 제목 타이포 계층 복원 (title2→title3)

--- AppProduct/AppProduct/Features/Home/Presentation/Components/Schedule/AttendancePolicyDisplaySection.swift
+++ AppProduct/AppProduct/Features/Home/Presentation/Components/Schedule/AttendancePolicyDisplaySection.swift
@@ -32,7 +32,7 @@ struct AttendancePolicyDisplaySection: View, Equatable {
     var body: some View {
         VStack(alignment: .leading, spacing: DefaultSpacing.spacing12) {
             Text("출석 정책")
-                .appFont(.title2Emphasis, color: .black)
+                .appFont(.title3Emphasis, color: .black)
 
             policyCard
         }
