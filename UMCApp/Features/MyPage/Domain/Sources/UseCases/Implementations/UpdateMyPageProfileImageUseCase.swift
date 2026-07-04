34555a4329df74a9e4f43ccd44cdfdf58ee3db9b
GthingkingG <190562383+GthingkingG@users.noreply.github.com>
2026-07-04 17:49:37 +0900

    refac : UpdateMyPageProfileImageUseCase이전

---
+++ UMCApp/Features/MyPage/Domain/Sources/UseCases/Implementations/UpdateMyPageProfileImageUseCase.swift
@@ -0,0 +1,33 @@
+//
+//  UpdateMyPageProfileImageUseCase.swift
+//  MyPage
+//
+//  Created by 김동민 on 7/4/26.
+//
+
+import Foundation
+
+public final class UpdateMyPageProfileImageUseCase: UpdateMyPageProfileImageUseCaseProtocol {
+    
+    // MARK: - Property
+    
+    private let repository: MyPageRepositoryProtocol
+    
+    // MARK: - Function
+    
+    public init(repository: MyPageRepositoryProtocol) {
+        self.repository = repository
+    }
+    
+    public func execute(
+        imageData: Data,
+        fileName: String,
+        contentType: String
+    ) async throws -> ProfileData {
+        try await repository.updateProfileImage(
+            imageData: imageData,
+            fileName: fileName,
+            contentType: contentType
+        )
+    }
+}
