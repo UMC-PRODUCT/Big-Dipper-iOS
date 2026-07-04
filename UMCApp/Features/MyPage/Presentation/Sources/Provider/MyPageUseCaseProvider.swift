0b3064dd9b14dacc1b5e42462902894265f6e8ff
GthingkingG <190562383+GthingkingG@users.noreply.github.com>
2026-07-04 17:33:13 +0900

    refac : Provider 구현

--- UMCApp/Features/MyPage/Presentation/Sources/Provider/MyPageUseCaseProvider.swift
+++ UMCApp/Features/MyPage/Presentation/Sources/Provider/MyPageUseCaseProvider.swift
@@ -17,20 +17,58 @@ import MyPageDomain
 public protocol MyPageUseCaseProviding {
     /// 내 프로필 조회 UseCase
     var fetchMyPageProfileUseCase: FetchMyPageProfileUseCaseProtocol { get }
+    /// 챌린저 연결 UseCase
+    var addChallngerRecordUseCase: AddChallengerRecordUseCaseProtocol { get }
+    /// 프로필 이미지변경 UseCase
+    var updateMyPageProfileImageUseCase: UpdateMyPageProfileImageUseCaseProtocol { get }
+    /// 회원 탈퇴 UseCase
+    var deletMemberUseCase: DeleteMemberUseCaseProtocol { get }
+    /// 내 글 조회 UseCase
+    var fetchMypostsUseCase: FetchMyPostsUseCaseProtocol { get }
+    /// 댓글 단 글들 조회 UseCase
+    var fetchMyCommentedPostsUseCase: FetchMyCommentedPostsUseCaseProtocol { get }
+    /// 스크랩한 글들 조회 UseCase
+    var fetchMyScrappedPostsUseCase: FetchMyScrappedPostsUseCaseProtocol { get }
+    /// 약관 조회 UseCase
+    var fetchTermsUseCase: FetchTermsUseCaseProtocol { get }
+    
 }
 
 /// `MyPageUseCaseProviding`의 기본 구현체.
 ///
 /// 단일 `MyPageRepositoryProtocol`을 주입받아 UseCase 인스턴스를 한 번씩 생성합니다.
 public final class MyPageUseCaseProvider: MyPageUseCaseProviding {
+    
+    
 
     // MARK: - Property
-
+    
     public let fetchMyPageProfileUseCase: FetchMyPageProfileUseCaseProtocol
-
-    // MARK: - Init
+    
+    public let addChallngerRecordUseCase: AddChallengerRecordUseCaseProtocol
+    
+    public let updateMyPageProfileImageUseCase: UpdateMyPageProfileImageUseCaseProtocol
+    
+    public let deletMemberUseCase: DeleteMemberUseCaseProtocol
+    
+    public let fetchMypostsUseCase: FetchMyPostsUseCaseProtocol
+    
+    public let fetchMyCommentedPostsUseCase: FetchMyCommentedPostsUseCaseProtocol
+    
+    public let fetchMyScrappedPostsUseCase: FetchMyScrappedPostsUseCaseProtocol
+    
+    public let fetchTermsUseCase: FetchTermsUseCaseProtocol
+    
+    // MARK: - Function
 
     public init(repository: MyPageRepositoryProtocol) {
-        self.fetchMyPageProfileUseCase = FetchMyPageProfileUseCase(repository: repository)
+//        self.fetchMyPageProfileUseCase = FetchMyPageProfileUseCase(repository: repository)
+//        self.addChallngerRecordUseCase = AddChallengerRecordUseCase(repository: repository)
+//        self.updateMyPageProfileImageUseCase = UpdateMyPageProfileImageUseCase(repository: repository)
+//        self.deletMemberUseCase = DeleteMemberUseCase(repository: repository)
+//        self.fetchMypostsUseCase = FetchMyPostsUseCase(repository: repository)
+//        self.fetchMyCommentedPostsUseCase = FetchMyCommentedPostsUseCase(repository: repository)
+//        self.fetchMyScrappedPostsUseCase = FetchMyScrappedPostsUseCase(repository: repository)
+//        self.fetchTermsUseCase = FetchTermsUseCase(repository: repository)
     }
 }
