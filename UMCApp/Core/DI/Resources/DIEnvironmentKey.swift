0fd13dc3e8f198b4f9f7a142ac5d27c912bbc490
GthingkingG <190562383+GthingkingG@users.noreply.github.com>
2026-07-04 11:26:50 +0900

    refac : DIEnvironmentKey 이전

--- UMCApp/Core/DI/Resources/DIEnvironmentKey.swift
+++ UMCApp/Core/DI/Resources/DIEnvironmentKey.swift
@@ -6,3 +6,28 @@
 //
 
 import Foundation
+import SwiftUI
+
+
+/// ### View에서 사용
+/// ```swift
+/// struct LoginView: View {
+///     @Environment(\.di) private var container
+///
+///     var body: some View {
+///         Button("로그인") {
+///             let useCase = container.resolve(LoginUseCaseProtocol.self)
+///             useCase.execute()
+///         }
+///     }
+/// }
+public struct DIEnvironmentKey: EnvironmentKey {
+    public static let defaultValue: DIContainer = .init()
+}
+
+extension EnvironmentValues {
+    public var di: DIContainer {
+        get { self[DIEnvironmentKey.self] }
+        set { self[DIEnvironmentKey.self] = newValue }
+    }
+}
