12228f74ea0d7a2048b69e23476130d8a7a32843
GthingkingG <190562383+GthingkingG@users.noreply.github.com>
2026-07-05 15:43:34 +0900

    refac : CorePhoto 이전, SinglePhotoPickerManageable 이전

---
+++ UMCApp/Core/Photo/Project.swift
@@ -0,0 +1,9 @@
+import ProjectDescription
+import ProjectDescriptionHelpers
+
+let project = coreProject(
+    name: "CorePhoto",
+    bundleIdSuffix: "photo",
+    dependencies: [
+    ]
+)
---
+++ UMCApp/Features/MyPage/Presentation/Sources/ViewModels/MyPageProfileViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  MyPageProfileViewModel.swift
+//  MyPage
+//
+//  Created by 김동민 on 7/5/26.
+//
+
+import Foundation
+import SwiftUI
+import PhotosUI
+
+/// 마이페이지 읽기 및 수정 화면의 비즈니스 로직을 담당하는 ViewModel입니다.
+///
+/// 프로필 데이터를 관리하고, 이미지 선택 및 업로드 동작을 처리합니다.
+@Observable
+public final class MyPageProfileViewModel: SinglePhotosPickerManageable {
+    
+}
