f3bb67738c9f010dc7242bcd618f2e03159f59a0
JEONG <ikejhc159@gmail.com>
2026-07-10 15:22:25 +0900

    ✨ [Feat] Firebase RemoteConfig SDK 도입 + Maintenance 모듈 스캐폴딩 (#946)
    - Tuist/Package.swift: firebase-ios-sdk 패키지 추가 (from: 12.7.0, 레거시
      AppProduct에서 검증된 버전과 동일한 하한 고정, 실제 해석은 12.16.0)
    - Features/Maintenance/Project.swift: featureProject로 Domain/Data/
      Presentation 3-타깃 모듈 생성. Data는 FirebaseCore/FirebaseRemoteConfig,
      Presentation은 CoreDI에 의존. Domain/Presentation 양쪽 테스트 타깃 포함
    - 루트 UMCApp/Project.swift: MaintenancePresentation/MaintenanceData +
      FirebaseCore 의존성 추가

--- UMCApp/Tuist/Package.swift
+++ UMCApp/Tuist/Package.swift
@@ -19,5 +19,8 @@ let package = Package(
         .package(url: "https://github.com/onevcat/Kingfisher.git", from: "8.6.1"),
         .package(url: "https://github.com/kakao/kakao-ios-sdk", from: "2.27.0"),
         .package(url: "https://github.com/google/GoogleSignIn-iOS", from: "9.1.0"),
+        // 원격 킬스위치(점검)·강제 업데이트(#946)의 RemoteConfig 소스.
+        // AppProduct(레거시)에서 검증된 버전(12.7.0)과 동일하게 고정한다.
+        .package(url: "https://github.com/firebase/firebase-ios-sdk", from: "12.7.0"),
     ]
 )
