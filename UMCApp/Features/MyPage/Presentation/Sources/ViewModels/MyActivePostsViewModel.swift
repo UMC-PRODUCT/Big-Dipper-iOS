e8cb45aca78f157e3a78b62acb89eb53be3e76a5
GthingkingG <190562383+GthingkingG@users.noreply.github.com>
2026-07-13 07:47:29 +0900

    refac : MyActiveLogsType이전

---
+++ UMCApp/Features/MyPage/Presentation/Sources/ViewModels/MyActivePostsViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  MyActivePostsViewModel.swift
+//  MyPage
+//
+//  Created by 김동민 on 7/13/26.
+//
+
+import Foundation
+
+@Observable
+public final class MyActivePostsViewModel {
+    // MARK: - Property
+    
+    private let logType: MyActiveLogsType
+}
